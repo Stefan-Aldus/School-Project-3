d132d24c02425479dc20d04d27364b7362b5f14e
Stefan <9018284@student.zadkine.nl>
2023-02-13 16:10:15 +0100

    Textd Adding Setup

--- index.php
+++ index.php
@@ -1,61 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Home</title>
-    <!-- Linking font for navbar logo -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Home</title>
+  <!-- Linking font for navbar logo -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
+
 <body>
-    <header class="main-head">
-      <!-- nav bar -->
+  <header class="main-head">
+    <!-- nav bar -->
     <?php include 'includes/nav.html'; ?>
   </header>
 
   <main class="bg">
-<section class="about-h">
-<div class="about-title-h">
-  <h1>Welkom bij Brouwerskazen</h1>
-  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus ducimus voluptatem excepturi laboriosam, doloribus totam minima cupiditate, culpa inventore vero error maiores? Hic repellat modi ullam, possimus laborum voluptas unde. Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim fuga dolores hic vero dolore sequi voluptatum obcaecati, aperiam consequuntur mollitia ex quasi non excepturi expedita dolorem nobis amet tempora possimus. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas sit commodi dolorum dolor impedit repellat ullam vitae sequi sint tenetur labore asperiores, necessitatibus cum consequuntur suscipit, assumenda ipsam iusto natus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ad error, nam id quas odio ea perferendis amet consectetur. Doloribus voluptatibus vitae officiis explicabo eveniet est error repellendus, eligendi tenetur. </p>
-</div>
+    <section class="about-h">
+      <div class="about-title-h">
+        <h1>Welkom bij Brouwerskazen</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus ducimus voluptatem excepturi laboriosam, doloribus totam minima cupiditate, culpa inventore vero error maiores? Hic repellat modi ullam, possimus laborum voluptas unde. Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim fuga dolores hic vero dolore sequi voluptatum obcaecati, aperiam consequuntur mollitia ex quasi non excepturi expedita dolorem nobis amet tempora possimus. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas sit commodi dolorum dolor impedit repellat ullam vitae sequi sint tenetur labore asperiores, necessitatibus cum consequuntur suscipit, assumenda ipsam iusto natus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ad error, nam id quas odio ea perferendis amet consectetur. Doloribus voluptatibus vitae officiis explicabo eveniet est error repellendus, eligendi tenetur. </p>
+      </div>
 
 
-<img class="about-pic" src="./img/tcrbuilding.jpg" alt="">
+      <img class="about-pic" src="./img/tcrbuilding.jpg" alt="">
 
 
-</section>
+    </section>
 
-<section class="header-h">
-    <div class="container-header-h">
-    <div class="card-h">
-        
-        </div>
-    
+    <section class="header-h">
+      <div class="container-header-h">
         <div class="card-h">
-            
+          <div class="card-h-detail"></div>
+        </div>
+        <p>Scherpe Kaas</p>
+
+        <div class="card-h">
+
+          <div class="card-h-detail"></div>
         </div>
         <div class="card-h">
-            
+
+          <div class="card-h-detail"></div>
         </div>
-    </div>
-<div class="container2-header-h">
-  <div class="card2-h">
+      </div>
+      <div class="container2-header-h">
+        <div class="card2-h">
 
-  </div>
-</div>
+        </div>
+      </div>
 
-</section>
+    </section>
 
 
 
   </main>
 
-<!-- footer -->
+  <!-- footer -->
   <?php include 'includes/footer.html'; ?>
 </body>
+
 </html>

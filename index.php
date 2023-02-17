03170148d2e24b8098679a94e2380bdd9e1ff87a
JensvanBrouwershaven <112871892+JensvanBrouwershaven@users.noreply.github.com>
2023-02-17 14:48:47 +0100

    homepage refined

--- index.php
+++ index.php
@@ -10,7 +10,9 @@
   <!-- Linking font for navbar logo -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet">
 </head>
 
 <body>
@@ -23,7 +25,15 @@
     <section class="about-h">
       <div class="about-title-h">
         <h1>Welkom bij Brouwerskazen</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus ducimus voluptatem excepturi laboriosam, doloribus totam minima cupiditate, culpa inventore vero error maiores? Hic repellat modi ullam, possimus laborum voluptas unde. Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim fuga dolores hic vero dolore sequi voluptatum obcaecati, aperiam consequuntur mollitia ex quasi non excepturi expedita dolorem nobis amet tempora possimus. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas sit commodi dolorum dolor impedit repellat ullam vitae sequi sint tenetur labore asperiores, necessitatibus cum consequuntur suscipit, assumenda ipsam iusto natus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ad error, nam id quas odio ea perferendis amet consectetur. Doloribus voluptatibus vitae officiis explicabo eveniet est error repellendus, eligendi tenetur. </p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus ducimus voluptatem excepturi
+          laboriosam, doloribus totam minima cupiditate, culpa inventore vero error maiores? Hic repellat modi ullam,
+          possimus laborum voluptas unde. Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim fuga dolores hic
+          vero dolore sequi voluptatum obcaecati, aperiam consequuntur mollitia ex quasi non excepturi expedita dolorem
+          nobis amet tempora possimus. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas sit commodi
+          dolorum dolor impedit repellat ullam vitae sequi sint tenetur labore asperiores, necessitatibus cum
+          consequuntur suscipit, assumenda ipsam iusto natus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Illo ad error, nam id quas odio ea perferendis amet consectetur. Doloribus voluptatibus vitae officiis
+          explicabo eveniet est error repellendus, eligendi tenetur. </p>
       </div>
 
 
@@ -35,22 +45,26 @@
     <section class="header-h">
       <div class="container-header-h">
         <div class="card-h">
-          <div class="card-h-detail"></div>
+
+          <p class="sharp-h">Scherpe Kaas</p>
+
         </div>
-        <p>Scherpe Kaas</p>
 
         <div class="card-h">
 
-          <div class="card-h-detail"></div>
+
+          <p class="sharp-h">Zachte Kaas</p>
         </div>
         <div class="card-h">
+          <p class="sharp-h">Smeltbare Kaas</p>
 
-          <div class="card-h-detail"></div>
+
         </div>
       </div>
       <div class="container2-header-h">
         <div class="card2-h">
 
+          <p class="sharp-h">Scherpe Kaas</p>
         </div>
       </div>
 

ebe15ba92db87c4a0c0e3a753697a1deef00651d
Stefan <9018284@student.zadkine.nl>
2023-03-07 11:13:44 +0100

    Mailscript en andere dingetje

--- Formulieren/productklacht-response.php
+++ Formulieren/productklacht-response.php
@@ -11,15 +11,26 @@
 </head>
 
 <body>
+
   <?php
   session_start();
-  ?>
 
+
+
+  if (isset($_POST["submit"])) {
+    $_SESSION["name"] = $_POST["name"];
+    $_SESSION["email"] = $_POST["email"];
+    $_SESSION["about"] = $_POST["about"];
+    $_SESSION["complaint"] = $_POST["complaint"];
+  } else {
+    echo "<h2>U bent niet op de juiste manier hier gekomen</h2>";
+
+  }
+  ?>
   <header class="main-head">
     <!-- nav bar -->
     <?php include '../includes/nav.html' ?>
   </header>
-
   <main class="bg">
     <div class="about-mk">
       <div class="about-mk-title">
@@ -30,6 +41,7 @@
         </p>
 
 
+
       </div>
       <img class="about-mk-pic" src="img/productklacht.png" alt="">
     </div>
@@ -37,20 +49,26 @@
     <form class="formulieren" method="post" action="./Formulieren/productklacht-response.php">
       <div class="input-mk">
         <label for="name">Uw naam is:</label>
-        <input class="textfield-1-mk type=" text" id="name" name="name" pattern="[A-Za-z]+" required />
+        <input class="textfield-1-mk type=" text" id="name" name="name" pattern="[A-Za-z]+"
+          value="<?php echo $_SESSION["name"] ?> " required />
         <label for="email">Uw email is:</label>
-        <input class="textfield-1-mk type=" text" id="email" name="email" required />
-        <label for="about:">Naam product:</label>
-        <input class="textfield-1-mk type=" text" id="about" name="about" required />
+        <input class="textfield-1-mk type=" text" id="email" name="email" value="<?php echo $_SESSION["email"] ?>"
+          required />
+        <label for="about:">Naam Product:</label>
+        <input class="textfield-1-mk type=" text" id="about" name="about" value=<?php echo $_SESSION["about"] ?>
+          required />
         <label for="complaint">Wat is uw klacht:</label>
-        <textarea name="complaint" id="complaint" required></textarea>
+        <textarea name="complaint" id="complaint" required><?php echo $_SESSION["complaint"]; ?></textarea>
         <input class="textfield-2-mk" type="submit" name="submit" id="submit" value="submit">
       </div>
     </form>
 
   </main>
 
-  <?php include '../includes/footer.html'; ?>
+  <?php
+  include '../includes/mailscript.php';
+  include '../includes/footer.html';
+   ?>
 </body>
 
 </html>

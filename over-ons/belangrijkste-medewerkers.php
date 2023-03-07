f7925c826d6e930d41f81d43fa07681c34963b10
Rowen Bakkeren <9018934@student.zadkine.nl>
2023-03-07 12:18:52 +0100

    Commitje

--- over-ons/belangrijkste-medewerkers.php
+++ over-ons/belangrijkste-medewerkers.php
@@ -1,17 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style.css">
-    <title>Document</title>
-    <base href="../">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../style.css">
+  <title>Document</title>
+  <base href="../">
 </head>
+
 <body>
-<header class="main-head">
+  <header class="main-head">
     <!-- nav bar -->
     <?php include '../includes/nav.html' ?>
   </header>
+
+  <main class="bg">
+    <section class="belangrijkstemedewerkers1">
+      <div class="belangrijkstemedewerkersstart">
+        <h1>Belangrijkste medewerkers</h1>
+        <p>Op deze pagina hebben we het over onze 4 belangrijkste medewerkers. Deze medewerkers hebben het bedrijf
+          opgericht en gemaakt zoals het nu is. Op deze pagina laten we informatie zien over deze medewerkers.</p>
+      </div>
+    </section>
+    <section class="medewerker-container">
+      <div class="medewerker">
+        <h2>Jens Brouwershaven</h2>
+        <p>Hallo mijn naam is Jens, ik ben 16 jaar oud en zit op de school Techniek College Rotterdam (verkort TCR).
+          Daarop doe ik software development, nu ben ik bezig met web development (daarmee maak je websites)
+          ik woon in Pernis Rotterdam en ik wil heel graag later iets met technologie doen, daarvoor wil ik naar het HBO
+          om verder te gaan met software developen.
+        </p>
+      </div>
+      <div class="medewerker">
+        <h2>Stefan Aldus</h2>
+        <p>Mijn naam is Stefan en ik zit op het Techniek College Rotterdam. Op deze studie heb ik PHP, Javascript,
+          HTML/CSS, en MySQL geleerd. Voor ons bedrijf heb ik deel van de PHP code geschreven, en de styling grotendeels
+          gemaakt. </p>
+        <p>Mijn woonplaats is Spijkenisse, en dit is ook waar vanuit ik werk aan de website en de scripting van
+          de website.</p>
+      </div>
+      <div class="medewerker">
+        <h2>Jesse Van Vliet</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam eaque voluptas, exercitationem explicabo,
+          nesciunt repellendus vel quia sint placeat officiis inventore deleniti iure, blanditiis omnis molestiae iste
+          itaque tempore beatae!</p>
+      </div>
+      <div class="medewerker">
+        <h2>Rowen Bakkeren</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi voluptatem similique ut tempora fugit
+          repudiandae omnis recusandae ratione qui, velit ab inventore possimus praesentium reiciendis blanditiis
+          eligendi saepe neque quam!
+        </p>
+      </div>
+
+
+    </section>
+  </main>
+
+  <?php
+  include '../includes/footer.html'
+    ?>
 </body>
+
 </html>

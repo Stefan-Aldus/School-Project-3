0e800ca48f28cd806db96862dfc29a521e3eb4b1
Jesse <9021012@student.zadkine.nl>
2023-03-13 16:13:33 +0100

    daily work

--- producten/Accesoires.php
+++ producten/Accesoires.php
@@ -13,7 +13,9 @@
 <body>
   <header class="main-head">
     <!-- nav bar -->
-    <?php include '../includes/nav.html' ?>
+    <?php include '../includes/nav.html';
+    require '../includes/connDatabase.php';
+    ?>
   </header>
 
   <main class="bg">
@@ -26,19 +28,71 @@
       <img class="about-pic" src="./img/kaasmerk-kazen.png" alt="">
     </section>
 
-
-    <section class="products-a">
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
+    <div class="flex-a">
+<section class="filter-a">
+<form method="post" action="">
+     <label for="1">Jong</label>      
+     <input type="radio"  name="1" value="1" />
+    
+</form>
+</section>
 
 
+    <section class="products-a small-margin">
+      <?php
+
+
+
+if (isset($_POST["submit"])) {
+
+}
+
+      // else {
+        try {
+        
+          $fullQuery = $db->prepare("SELECT products.*, category.name AS Category, suppliers.name AS Supplier FROM products INNER JOIN category ON category.categoryid = products.categoryid INNER JOIN suppliers ON products.supplierid = suppliers.supplierid ");
+        } catch (PDOexception $e) {
+          die("Fout bij verbinden met database: " . $e->getMessage());
+        }
+      // }
+
+      #3 query uitvoeren
+      $fullQuery->execute();
+
+      #4 checken of er een resultaat is
+      // echo "aantal regels is: " . $fullQuery->rowCount();
+      $result = $fullQuery->fetchall(PDO::FETCH_ASSOC);
+
+      #5 resultaat op scherm tonen
+      ?>
+      <?php
+
+      if ($fullQuery->rowCount() > 0) {
+
+        foreach ($result as $product) {
+          # filmnaam, regisseur, releasejaar etc moeten gelijk zijn aan de namen die gebruikt zijn in de query
+          echo "<div>" . "
+                    <h3>Naam: " . $product["name"] . "</h3>
+                    <ul>
+                      <li>Leeftijd: " . $product["age"] . " maanden oud</li>
+                      <li>Type: " . $product["type"] . "</li>
+                      <li>Flavor: " . $product["flavor"] . "</li>
+                      <li>Texture: " . $product["texture"] . "</li>
+                      <li>Prijs: $" . $product["price"] . " per kilogram</li>
+                      <li>Supplier: " . $product["Supplier"] . "</li>
+                      <li>Category: " . $product["Category"] . "</li>
+                    </ul>
+                  </div>";
+
+        }
+
+      } else {
+        echo "Geen resultaten gevonden";
+      }
+
+      ?>
     </section>
+  </div>
   </main>
 
 </body>

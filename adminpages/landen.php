29e0467a067fc1034a4c37c7d41ec07877d65315
Stefan <9018284@student.zadkine.nl>
2023-03-21 11:10:08 +0100

    uhh

--- adminpages/landen.php
+++ adminpages/landen.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <base href="../">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Landen inzien - Brouwerskazen</title>
 </head>
 
 <body>

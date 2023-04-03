0beb3659242b48b0c936f96b5a669849bec792ab
Rowen Bakkeren <9018934@student.zadkine.nl>
2023-04-03 15:29:33 +0200

    IHATESQLLL

--- adminpages/avgpriceleveranciers.php
+++ adminpages/avgpriceleveranciers.php
@@ -10,14 +10,14 @@
   <title>Document</title>
 </head>
 
-<body></body>
+<body>
   <header class="main-head">
     <!-- nav bar -->
     <?php include '../includes/nav.html' ?>
   </header>
 
   <main class="bg">
-  <div class="about-mk">
+    <div class="about-mk">
       <div class="about-mk-title">
         <h1>Gemiddelde Prijs Leveranciers</h1>
         <p> Op deze pagina kunt u de gemiddelde productprijs van elke leverancier inzien</p>
@@ -25,57 +25,56 @@
       </div>
       <img class="about-mk-pic" src="./img/leverancier.png" alt="">
     </div>
-   <section class="order-list2 small-margin">
-    
-
-   <?php
-    # uitleg jesse
-    # require maakt connectie met database
-    require "../includes/connDatabase.php";
-
-    # een query maken en proberen
-    try {
-      $fullQuery = $db->prepare("SELECT DISTINCT supplierid, AVG(price) as avgprice FROM products GROUP BY supplierid");
-    }
-    # de catch als hij niet werkt
-    catch (PDOexception $e) {
-      die("Fout bij verbinden met database: " . $e->getMessage());
-    }
+    <section class="order-list2 small-margin">
 
 
-    #stap 3 de quarry uit voeren
-    $fullQuery->execute();
-    $result = $fullQuery->fetchall(PDO::FETCH_ASSOC);
-  ?>
+      <?php
+      # uitleg jesse
+      # require maakt connectie met database
+      require "../includes/connDatabase.php";
 
-<table>
-        <thead>
-            <th>Id leverancier</th>
-            <th>Gemiddelde prijs products</th>
-        </thead>
-        <tbody></tbody>
- 
-  <?php
-    # checken of er resultaat is
-    if ($fullQuery->rowCount() > 0) {
-      # voor elke regel in phpmyadmind sql database result
-      foreach ($result as $row) {
-        echo "<tr><td>" . $row["supplierid"] ."</td>";
-        echo "<td>" . $row["avgprice"] ."</td></tr>";
+      # een query maken en proberen
+      try {
+        $fullQuery = $db->prepare("SELECT DISTINCT supplierid, AVG(price), FORMAT(AVG(price) ,2) as avgprice FROM products GROUP BY supplierid");
+      }
+      # de catch als hij niet werkt
+      catch (PDOexception $e) {
+        die("Fout bij verbinden met database: " . $e->getMessage());
       }
 
-    }
 
-    else 
-            {
-              echo  "Geen resultaten gevonden";
+      #stap 3 de quarry uit voeren
+      $fullQuery->execute();
+      $result = $fullQuery->fetchall(PDO::FETCH_ASSOC);
+      ?>
+
+      <table>
+        <thead>
+          <th>Id leverancier</th>
+          <th>Gemiddelde prijs products</th>
+        </thead>
+        <tbody>
+
+          <?php
+          # checken of er resultaat is
+          if ($fullQuery->rowCount() > 0) {
+            # voor elke regel in phpmyadmind sql database result
+            foreach ($result as $row) {
+              echo "<tr><td>" . $row["supplierid"] . "</td>";
+              echo "<td>" . $row["avgprice"] . "</td></tr>";
             }
-    ?>
-   </section>
-   
-   
 
-    
+          } else {
+            echo "Geen resultaten gevonden";
+          }
+          ?>
+        </tbody>
+        </table>
+    </section>
+
+
+
+
   </main>
   <?php include '../includes/footer.html'; ?>
 </body>
